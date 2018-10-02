bcf38c7c6018e3993f1d20998414379b8d673967
Christian Mejia <chrismejia10@gmail.com>
2018-10-01 20:36:28 -0400

    Final code added tonight

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Test = require("../db/models/test");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const tests = await Test.findAll();
+    res.send(tests);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let singleTest = await Test.findById(req.params.id);
+    res.send(singleTest);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/student/:id", async function(req, res, next) {
+  try {
+    let newTest = await Test.create({where:
+
+    }req.body);
+    res.status(201).send(newTest);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
